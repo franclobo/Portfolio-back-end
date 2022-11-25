564a32c598aa56edbd0f4de48862a6ac2a926ca4
franclobo <fjbl2788@gmail.com>
2022-11-25 11:03:09 -0500

    Config to deploy with render

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::ProjectsController < ApplicationController
   def index
     @projects = Project.all
 
-    render json: @projects
+    render json: @projects.to_json
   end
 
   # GET /projects/1
