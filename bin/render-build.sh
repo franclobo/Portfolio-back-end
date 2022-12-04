d468cefc7c5585ee6fc9a8133e73b2db40de1bcb
franclobo <fjbl2788@gmail.com>
2022-12-04 16:43:56 -0500

    Configure Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,8 @@
 set -o errexit
 
 bundle install
-REVOKE CONNECT ON DATABASE thedb FROM public;
+bundle exec REVOKE CONNECT ON DATABASE thedb FROM public;
 bundle exec rake db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-GRANT CONNECT ON DATABASE thedb TO public;
+bundle exec GRANT CONNECT ON DATABASE thedb TO public;
 bundle exec rake db:create
 bundle exec rake db:migrate
